af2252c0e1e6574c36487526e681f9a4e582d474
camila <camilasangobar@gmail.com>
2021-03-21 17:33:49 -0300

    areatriangulo

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace trianguloarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = 3.0;
+            double h = 5.0;
+            double a = b * h/2;
+            Console.WriteLine($"base:{b}");
+            Console.WriteLine($"altura:{h}");
+            Console.WriteLine($"area:{a}");
+            
+
+           
+
+
+            
+            
+            
+        }
+    }
+}
